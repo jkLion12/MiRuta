cedaab535657de2879c1dd7326ece331377158ec
Jack Huamani <jackhuamani25@gmail.com>
2026-05-11 15:02:46 -0500

    corrigiendo bug de navegacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { IonApp, IonIcon, IonRouterOutlet } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  gridOutline,
+  navigateOutline,
+  settingsOutline,
+  timeOutline,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  styleUrl: 'app.component.scss',
+  imports: [IonApp, IonIcon, IonRouterOutlet, RouterLink, RouterLinkActive],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    addIcons({
+      'grid-outline': gridOutline,
+      'navigate-outline': navigateOutline,
+      'time-outline': timeOutline,
+      'settings-outline': settingsOutline,
+    });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,35 +3,33 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./paginas/principal/principal.page').then((m) => m.PrincipalPage),
-    children: [
-      {
-        path: '',
-        redirectTo: 'panel',
-        pathMatch: 'full',
-      },
-      {
-        path: 'panel',
-        loadComponent: () => import('./paginas/panel/panel.page').then((m) => m.PanelPage),
-      },
-      {
-        path: 'viajes',
-        loadComponent: () => import('./paginas/viajes/viajes.page').then((m) => m.ViajesPage),
-      },
-      {
-        path: 'historial',
-        loadComponent: () => import('./paginas/historial/historial.page').then((m) => m.HistorialPage),
-      },
-      {
-        path: 'configuracion',
-        loadComponent: () =>
-          import('./paginas/configuracion/configuracion.page').then((m) => m.ConfiguracionPage),
-      },
-    ],
+    redirectTo: 'panel',
+    pathMatch: 'full',
+  },
+  {
+    path: 'panel',
+    loadComponent: () => import('./paginas/panel/panel.page').then((m) => m.PanelPage),
+  },
+  {
+    path: 'viajes',
+    loadComponent: () => import('./paginas/viajes/viajes.page').then((m) => m.ViajesPage),
+  },
+  {
+    path: 'historial',
+    loadComponent: () => import('./paginas/historial/historial.page').then((m) => m.HistorialPage),
+  },
+  {
+    path: 'configuracion',
+    loadComponent: () =>
+      import('./paginas/configuracion/configuracion.page').then((m) => m.ConfiguracionPage),
   },
   {
     path: 'historial/:id',
     loadComponent: () =>
       import('./paginas/detalle-viaje/detalle-viaje.page').then((m) => m.DetalleViajePage),
+  },
+  {
+    path: '**',
+    redirectTo: 'panel',
   },
 ];
